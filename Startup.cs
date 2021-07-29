fe76f089f9542b1c15427b85c561042b4403c092
tieuanhquoc <tieuanhquoc@gmail.com>
2021-07-29 13:59:38 +0700

    ád

--- Startup.cs
+++ Startup.cs
@@ -36,19 +36,17 @@ namespace TestLoadBalancer
             }, ServiceLifetime.Transient);
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "TestLoadBalancer", Version = "v1"});
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TestLoadBalancer", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestLoadBalancer v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestLoadBalancer v1"));
 
             app.UseHttpsRedirection();
 
